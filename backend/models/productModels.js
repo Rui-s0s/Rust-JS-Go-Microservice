9364c080dd390edfacb15b485c7533306d704470
Rui Segovia <segovia.rodrigo2002@gmail.com>
2026-02-24 18:59:13 -0300

    feat: Modelo basico para base de datos mongo

---
+++ backend/models/productModels.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: Number
+});
+
+export const Product = mongoose.model('Product', productSchema);
