7e8c0f961fba02f1bcf0baa3cd72686a0411d6f6
Rui Segovia <segovia.rodrigo2002@gmail.com>
2026-02-25 19:14:04 -0300

    feat: Forms y pug como view engine

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,11 @@ import productRoutes from './routes/productRoutes.js';
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
+app.set('view engine', 'pug')
 
 await connectDB();
 
-app.use("/products", productRoutes);
+app.use("/", productRoutes);
 
 app.listen(3000, () => console.log('Server at http://localhost:3000'));
