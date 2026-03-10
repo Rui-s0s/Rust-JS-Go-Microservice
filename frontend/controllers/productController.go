c3616eadc069a466790492de999291496016e0c1
Rui Segovia <segovia.rodrigo2002@gmail.com>
2026-03-10 20:09:27 -0300

    style: Espaciado y documentacion

--- frontend/controllers/productController.go
+++ frontend/controllers/productController.go
@@ -18,33 +18,38 @@ type ProductController struct {
 
 // r request w response (r what you "r"ead, w what you "w"rite as a response)
 
+// For "/products"
 func (pc *ProductController) ListProducts(w http.ResponseWriter, r *http.Request) {
+	// ctx checks the status of the conection
 	ctx := r.Context()
 
 	products, err := models.GetAllProducts(ctx, pc.ProductCollection)
 	if err != nil {
-		http.Error(w, err.Error(), 500)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	// Loads the html and sends the data along with it
 	tmpl := template.Must(template.ParseFiles("views/templates/index.html"))
 	tmpl.Execute(w, products)
 }
 
+// For "/product/"
 func (pc *ProductController) GetProduct(w http.ResponseWriter, r *http.Request) {
-	// URL expected: /product/{id}
-	idParam := r.URL.Path[len("/product/"):] // strip prefix
+	// Getting the id from url
+	idParam := r.URL.Path[len("/product/"):]
 	if idParam == "" {
 		http.NotFound(w, r)
 		return
 	}
 
+	// Converting to ObjectID for mongo to use
 	objID, err := primitive.ObjectIDFromHex(idParam)
 	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
-
+	// Calls the struct that we defined to hold the data that we're going to send
 	var product models.Product
 	err = pc.ProductCollection.FindOne(r.Context(), bson.M{"_id": objID}).Decode(&product)
 	if err != nil {
@@ -52,30 +57,30 @@ func (pc *ProductController) GetProduct(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
+	// Loads the html and sends the data along with it
 	tmpl := template.Must(template.ParseFiles("views/templates/product.html"))
 	tmpl.Execute(w, product)
 }
 
+// For "/product/order/"
 func (pc *ProductController) PlaceOrder(w http.ResponseWriter, r *http.Request) {
-	// 1. Extract Product ID from URL
+	// Getting the id and converting to ObjectID for mongo to use
 	idParam := r.URL.Path[len("/product/order/"):]
 	productID, _ := primitive.ObjectIDFromHex(idParam)
 
-	// 2. The "Atomic" Stock Decrease
-	// Only update IF the ID matches AND stock is greater than 0
+	// Check if the stock is greater than 0 then decrease it by one
 	filter := bson.M{"_id": productID, "stock": bson.M{"$gt": 0}}
 	update := bson.M{"$inc": bson.M{"stock": -1}}
 
 	result, err := pc.ProductCollection.UpdateOne(r.Context(), filter, update)
 
-	// If ModifiedCount is 0, it means either the ID was wrong
-	// or (more likely) the stock was already 0.
+	// If we get any errors when updating the database we respond to the client
 	if err != nil || result.ModifiedCount == 0 {
 		http.Error(w, "Item is out of stock!", http.StatusConflict)
 		return
 	}
 
-	// 3. Create the Order Record
+	// Create a new order
 	newOrder := bson.M{
 		"product_id": productID,
 		"email":      r.FormValue("email"),
@@ -83,10 +88,9 @@ func (pc *ProductController) PlaceOrder(w http.ResponseWriter, r *http.Request) 
 		"created_at": time.Now(),
 	}
 
+	// If an error happens while creating an order we increment the value for that product
 	_, err = pc.OrderCollection.InsertOne(r.Context(), newOrder)
 	if err != nil {
-		// ERROR HANDLING: If the order fails to save, we should
-		// give the stock back! (A "Rollback")
 		pc.ProductCollection.UpdateOne(r.Context(), bson.M{"_id": productID}, bson.M{"$inc": bson.M{"stock": 1}})
 		http.Error(w, "Failed to create order", http.StatusInternalServerError)
 		return
@@ -95,6 +99,7 @@ func (pc *ProductController) PlaceOrder(w http.ResponseWriter, r *http.Request) 
 	http.Redirect(w, r, "/order-success", http.StatusSeeOther)
 }
 
+// For "/order-success"
 func (pc *ProductController) OrderSuccess(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("views/templates/success.html"))
 	tmpl.Execute(w, nil)
--- frontend/models/product.go
+++ frontend/models/product.go
@@ -17,13 +17,17 @@ type Product struct {
 }
 
 // Context works like a signal to not waste resources, this is get all like db.products.find({})
+
 func GetAllProducts(ctx context.Context, col *mongo.Collection) ([]Product, error) {
 	cursor, err := col.Find(ctx, bson.M{})
 	if err != nil {
 		return nil, err
 	}
+
+	// After everything ends close the connection
 	defer cursor.Close(ctx)
 
+	// Call our struct and fetch all the data
 	var products []Product
 	err = cursor.All(ctx, &products)
 	return products, err
