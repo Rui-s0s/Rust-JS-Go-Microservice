91da48d53b7b487410e1062785c945d348b87f96
Rui Segovia <segovia.rodrigo2002@gmail.com>
2026-03-11 20:27:35 -0300

    feat: Modificacion para que el esquema use state

--- backend/models/productModels.js
+++ backend/models/productModels.js
@@ -4,6 +4,7 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true }, 
   price: { type: Number, required: true},
   stock: { type: Number, required: true},
+  state: { type: String, default: "In Stock"},
 });
 
 export const Product = mongoose.model('Product', productSchema);
